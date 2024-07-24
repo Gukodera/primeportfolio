72513e19a6261fd54d24502fbaf4e5cdd98f584d
prime <primeborres@gmail.com>
2024-07-24 11:42:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing", {
+    strings: [,`BSIT STUDENT`,`Application Developer`, `Web designer.`, `Editor.`, `Web developer.`,`BSIT STUDENT`,],
+    typeSpeed: 100,
+    BackSpeed: 80,
+    loop:true
+})
+
+
+
+
